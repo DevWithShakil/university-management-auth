b53c6d96d748803fdd6bc5ea433813f60bacdf48
Shakil Khan <m.shakilkhan720@gmail.com>
2023-06-04 22:14:07 +0600

    check

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
+import usersRouter from './app/modules/users/users.route'
 const app: Application = express()
 
 app.use(cors())
@@ -8,10 +9,13 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+// Application routes
+
+app.use('/api/v1/users/', usersRouter)
 
 //Testing
 app.get('/', async (req: Request, res: Response) => {
-    res.send('Working Successfully')
+  res.send('Working Successfully')
 })
 
 export default app
