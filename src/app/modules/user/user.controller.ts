78dba33104f03dc3c83e42d05782692f5021a46e
Shakil Khan <m.shakilkhan720@gmail.com>
2023-06-24 12:52:22 +0600

    module 15.5 till all integrated

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -4,10 +4,11 @@ import catchAsync from '../../../shared/catchAsync';
 import sendResponse from '../../../shared/sendResponse';
 import { UserService } from './user.services';
 
-const createUser: RequestHandler = catchAsync(
+const createStudent: RequestHandler = catchAsync(
   async (req: Request, res: Response) => {
-    const { user } = req.body;
-    const result = await UserService.createUser(user);
+    const { student, ...userData } = req.body;
+
+    const result = await UserService.createStudent(student, userData);
     sendResponse(res, {
       statusCode: httpStatus.OK,
       success: true,
@@ -18,5 +19,5 @@ const createUser: RequestHandler = catchAsync(
 );
 
 export const UserController = {
-  createUser,
+  createStudent,
 };
